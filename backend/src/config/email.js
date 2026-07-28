832c8b28cec099577397dcb541b694889869d08d
Stevejnr03 <128726367+Stevejnr03@users.noreply.github.com>
2026-07-28 04:08:17 +0100

    Update email.js

--- backend/src/config/email.js
+++ backend/src/config/email.js
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  // host: process.env.EMAIL_HOST,
-  // port: process.env.EMAIL_PORT,
-  service: "gmail"
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
   secure: false,
+  family: 4, // Use IPv4
+  tls: {
+    rejectUnauthorized: false,
+  },
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
